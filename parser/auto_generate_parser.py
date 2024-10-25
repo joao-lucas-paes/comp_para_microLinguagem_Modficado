0faa03c4f121c7a508e3a7fa0d355c0200cfbc57
joao-lucas-paes <joaozinhopaes12@gmail.com>
2024-10-24 21:34:39 -0400

    updated Generate parser auto

--- parser/auto_generate_parser.py
+++ parser/auto_generate_parser.py
@@ -1,7 +1,9 @@
 import json
 import argparse
+from numpy import NAN
 import ply.yacc as yacc
-
+import math
+import pandas as pd
 
 args = argparse.ArgumentParser()
 args.add_argument("--grammar-path", default="./grammar.json", type=str, required=False)
@@ -13,15 +15,57 @@ with open(grammar_path, 'r') as f:
 
 tokens = tuple(grammar["tokens"])
 
-print(tokens)
-
 productions = grammar["states"]
+states = list(productions.keys())
+n_prod = 0
+
+t_len = int(
+    math.log10(
+        sum([
+            len(productions[state]) for state in productions.keys()
+            ]))) + 1
+
+format_mode = f"0{t_len}d"
+rules = []
+
 for state in productions.keys():
     for index, rule in enumerate(productions[state]):
-        name_prod = f"p_{state}_{index}"
-        func = (f"def {name_prod}(p):\n" + 
+        name_prod = f"p_n{n_prod:0{t_len}d}_{state}_{index}"
+        n_prod += 1
+        rules.append(f'{state} -> {rule}')
+        func = (f"def {name_prod} (p):\n"  + 
                 f"    '{state} : {rule}'\n"+
                 f"    pass")
         exec(func)
 
-yacc.yacc()
+parser = yacc.yacc("SLR")
+action = parser.action
+goto = parser.goto
+
+def join_action(index, action, rule_row):
+    if index in action:
+        row = action[index]
+        for symbols in row.keys():
+            rule_row[symbols] = f"s{abs(row[symbols])}"
+            if row[symbols] < 0:
+                rule_row[symbols] = f"r{abs(row[symbols])}"
+
+def join_goto(index, goto, rule_row):
+    if index in goto:
+        rule_row |= goto[index]
+
+rule_action_table = []
+rule_goto_table = []
+
+for index in range(max(list(action.keys()) + list(action.keys()))):
+    rule_row_goto = {"prod_name":index}
+    rule_row_action = {"prod_name":index}
+    join_action(index, action, rule_row_action)
+    join_goto(index, goto, rule_row_goto)
+    rule_action_table.append(rule_row_action)
+    rule_goto_table.append(rule_row_goto)
+
+table_action = pd.DataFrame(rule_action_table)
+table_action.to_csv("./action.csv")
+table_goto = pd.DataFrame(rule_goto_table).fillna(-1).astype(int)
+table_goto.to_csv("./goto.csv")
